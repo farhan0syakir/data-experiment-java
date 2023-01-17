8bfb2ba4a8733c3db9ce1ed05a80645b8a5e7558
farhan.syakir <farhan0syakir@gmail.com>
2023-01-17 20:22:37 +0200

    optimize import

--- kafka/src/main/java/com/example/demo/WordCountRestService.java
+++ kafka/src/main/java/com/example/demo/WordCountRestService.java
@@ -8,8 +8,6 @@ import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
